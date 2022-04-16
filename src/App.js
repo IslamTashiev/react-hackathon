1d990a674fbeb6cc74d9e22842afb2d024392e5a
AidaGube <aidagube@gmail.com>
2022-04-16 22:59:40 +0300

    contacts form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CardPage from "./views/CardPage";
+import ContactPage from "./views/ContactPage";
 import { HomePage } from "./views/HomePage";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/card" element={<CardPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </div>
   );
