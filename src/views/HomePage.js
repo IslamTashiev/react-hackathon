36ac757f4234d9646d8d159233ef5ebacd59ac98
AidaGube <aidagube@gmail.com>
2022-04-18 21:42:54 +0300

    adaptiv

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
-// import { JustSlider } from "../components/JustSlider/JustSlider";
+import { JustSlider } from "../components/JustSlider/JustSlider";
 
 export const HomePage = () => {
   return (
     <div className='wrapper'>
       <Header />
-      {/* <JustSlider /> */}
+      <JustSlider />
       <Footer />
     </div>
   );
