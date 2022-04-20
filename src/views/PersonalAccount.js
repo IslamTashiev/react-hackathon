382dadd9d6aed7b3124e00b922486bbabb1ad7bd
islam <islam.maratov13@gmil.com>
2022-04-21 00:36:06 +0600

    add personalAccount

--- src/views/PersonalAccount.js
+++ src/views/PersonalAccount.js
@@ -2,12 +2,37 @@ import React from "react";
 import Account from "../components/PersonalAccount/Account";
 import AccountSideBar from "../components/PersonalAccount/AccountSideBar";
 
+const items = [
+  {
+    id: 1,
+    content: "Общие сведения",
+  },
+  {
+    id: 2,
+    content: "Личные данные",
+  },
+  {
+    id: 3,
+    content: "История покупок",
+  },
+  {
+    id: 4,
+    content: "Избранное",
+  },
+  {
+    id: 5,
+    content: "Сменить пароль",
+  },
+];
+
+
+
 const PersonalAccount = () => {
   return (
     <div className="container">
       <h1>Общие сведения</h1>
       <div className="personal-account">
-        <AccountSideBar />
+        <AccountSideBar items={items} />
         <Account />
       </div>
     </div>
