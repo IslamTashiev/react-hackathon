36814593d20648e9f5d1519deeb14e335d9bd2b3
solohad1n <fox663399@gmail.com>
2022-04-22 20:00:45 +0600

    made in the account profile: 1.General information. 2.Personal data. 3.Purchase history 4.Change password.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CardPage from "./views/CardPage";
 import ContactPage from "./views/ContactPage";
+import ProfileMain from './views/ProfileMain';
+import ProfilePerconalData from './views/ProfilePerconalData';
 import { HomePage } from "./views/HomePage";
+import ProfileHistory from "./views/ProfileHistory";
+import ProfileChangePass from "./views/ProfileChangePass";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/card' element={<CardPage />} />
         <Route path='/contact' element={<ContactPage />} />
+        <Route path="/profilemain" element={<ProfileMain />} />
+        <Route path="/perconaldata" element={<ProfilePerconalData />} />
+        <Route path="/profilehistory" element={<ProfileHistory />} />
+        <Route path="/profilechange" element={<ProfileChangePass />} />
       </Routes>
     </div>
   );
