e008668c38878ac012fc1f181e2e899a1898c20b
m-uson <usonibrahimovic@gmai.com>
2022-05-04 14:50:15 +0600

    addded

--- src/components/Catalog/ProductList.js
+++ src/components/Catalog/ProductList.js
@@ -1,39 +1,39 @@
 import React, { useContext, useEffect } from "react";
 import { appContext } from "../../context/appContext";
-import { ProductItem } from "./ProductItem";
-import arrowIcon from "../../assets/images/rigth-arrow.svg";
+import { ProductItem } from "../ProductList/ProductItem";
 import { Oval } from "react-loader-spinner";
-
 import "../ProductList/style.css";
 
 export const ProductList = () => {
-  const { products, fetchProducts } = useContext(appContext);
+  const { products, getProductsFromFirebase } = useContext(appContext);
 
   useEffect(() => {
-    fetchProducts();
+    getProductsFromFirebase();
   }, []);
 
   return (
     <div className="products__list-content">
-      <div className="products__list">
-        {products.length ? (
-          products.map((item) => (
-            <React.Fragment key={item.id}>
-              <ProductItem product={item} />
-            </React.Fragment>
-          ))
-        ) : (
-          <div className="loader">
-            <Oval
-              ariaLabel="loading-indicator"
-              height={100}
-              width={100}
-              strokeWidth={3}
-              color="#2A5275"
-              secondaryColor="#838688"
-            />
-          </div>
-        )}
+      <div className="container">
+        <div className="products__list">
+          {products.length ? (
+            products.map((item) => (
+              <React.Fragment key={item.id}>
+                <ProductItem product={item} />
+              </React.Fragment>
+            ))
+          ) : (
+            <div className="loader">
+              <Oval
+                ariaLabel="loading-indicator"
+                height={100}
+                width={100}
+                strokeWidth={3}
+                color="#2A5275"
+                secondaryColor="#838688"
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
